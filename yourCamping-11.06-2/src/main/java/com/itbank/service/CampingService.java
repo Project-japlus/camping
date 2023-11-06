ef43570d36a81b2216eee172bff8500751328e61
RightAccept <djfltrhkdeo46@gmail.com>
2023-11-06 19:39:04 +0900

    1차 백업 이름 변경, 1차 백업에 조금 수정된거

---
+++ yourCamping-11.06-2/src/main/java/com/itbank/service/CampingService.java
@@ -0,0 +1,224 @@
+package com.itbank.service;
+
+import java.util.HashMap;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.itbank.component.FileComponent;
+import com.itbank.model.CampingDTO;
+import com.itbank.repository.CampingDAO;
+
+@Service
+public class CampingService {
+
+	@Autowired
+	private CampingDAO campingDAO;
+	@Autowired
+	private FileComponent fileComponent;
+
+	public int getListCnt() {
+		int cnt = campingDAO.getListCnt();
+		return cnt;
+	}
+
+	public List<CampingDTO> getCampingList(HashMap<String, Object> params) {
+		List<CampingDTO> list = campingDAO.getCampingList(params);
+		return list;
+	}
+
+	// 대표 이미지
+	public CampingDTO selectOne(int camping_idx) {
+		return campingDAO.selectOne(camping_idx);
+	}
+
+	// 상세 이미지
+	public List<CampingDTO> selectOneImage(int camping_idx) {
+		return campingDAO.selectOneImage(camping_idx);
+	}
+
+	// 검색어/업종이 있을 때 개수 구하기
+	public int getSearchCnt(HashMap<String, String> param) {
+		return campingDAO.getSearchCnt(param);
+	}
+
+	public int campingInsert(CampingDTO dto) {
+		if (dto.getInduty() == null) {
+			dto.setInduty("일반야영장");
+		}
+		return campingDAO.campingInsert(dto);
+	}
+
+	public int maxCampingIdx() {
+		return campingDAO.maxCampingIdx();
+	}
+
+	public int campingImgInsert(CampingDTO dto) {
+		if (dto.getUpload1().getSize() != 0) {
+			String fileName = fileComponent.upload(dto.getUpload1());
+			dto.setFirst_img(fileName);
+		}
+		if (dto.getUpload2() != null) {
+			for (int i = 0; i < dto.getUpload2().length; i++) {
+				String fileName = fileComponent.upload2(dto.getUpload2()[i]);
+				CampingDTO inner = new CampingDTO();
+				inner.setCamping_idx(dto.getCamping_idx());
+				inner.setInner_img(fileName);
+				campingDAO.campingImgInsert(inner);
+			}
+		}
+		return campingDAO.campingImgInsert(dto);
+	}
+
+	public int campingPlaceInsert(CampingDTO dto) {
+		return campingDAO.campingPlaceInsert(dto);
+	}
+
+	public int activityInsert(CampingDTO dto) {
+		if (dto.getExprnprogrm() != null) {
+			dto.setExprnprogrmat("Y");
+		} else {
+			dto.setExprnprogrmat("N");
+		}
+		return campingDAO.activityInsert(dto);
+	}
+
+	public int introduceInsert(CampingDTO param) {
+		return campingDAO.introduceInsert(param);
+	}
+
+	public int internalInsert(CampingDTO dto) {
+		return campingDAO.internalInsert(dto);
+	}
+
+	public int safetyDeviceInsert(CampingDTO dto) {
+		return campingDAO.safetyDeviceInsert(dto);
+	}
+
+	public int campingSiteInsert(CampingDTO dto) {
+		return campingDAO.campingSiteInsert(dto);
+	}
+
+	public int updateCamping(CampingDTO dto) {
+		return campingDAO.updateCamping(dto);
+	}
+
+	public int updateCampingImg(CampingDTO dto) {
+		if (dto.getUpload1().getSize() != 0) {
+			String fileName = fileComponent.upload(dto.getUpload1());
+			dto.setFirst_img(fileName);
+		}
+		if (dto.getUpload2() != null) {
+			for (int i = 0; i < dto.getUpload2().length; i++) {
+				String fileName = fileComponent.upload2(dto.getUpload2()[i]);
+				CampingDTO inner = new CampingDTO();
+				inner.setCamping_idx(dto.getCamping_idx());
+				inner.setInner_img(fileName);
+				campingDAO.updateCampingImg(inner);
+			}
+		}
+
+		return campingDAO.updateCampingImg(dto);
+	}
+
+	public int updateInfoTwo(CampingDTO dto) {
+		return campingDAO.updateInfoTwo(dto);
+	}
+
+	public int updateActivity(CampingDTO dto) {
+		if (dto.getExprnprogrm() != null) {
+			dto.setExprnprogrmat("Y");
+		} else {
+			dto.setExprnprogrmat("N");
+		}
+		return campingDAO.updateActivity(dto);
+	}
+
+	public int updateIntroduce(CampingDTO dto) {
+		return campingDAO.updateIntroduce(dto);
+	}
+
+	// 이전
+	public int deleteCamping(int maxCampingIdx) {
+		return campingDAO.deleteCamping(maxCampingIdx);
+	}
+
+	public int deleteCampingImg(CampingDTO dto) {
+		fileComponent.deleteFile(dto.getFirst_img());
+		String[] fileName = dto.getInner_img().split(",");
+		for (int i = 0; i < fileName.length; i++) {
+			fileComponent.deleteFile2(fileName[i]);
+		}
+		return campingDAO.deleteCampingImg(dto);
+	}
+
+	public int deleteCampingActivity(int maxCampingIdx) {
+		return campingDAO.deleteCampingActivity(maxCampingIdx);
+	}
+
+	public int deleteCampingIntoduce(int maxCampingIdx) {
+		return campingDAO.deleteCampingIntoduce(maxCampingIdx);
+	}
+
+	public int deleteCampingPlace(int maxCampingIdx) {
+		return campingDAO.deleteCampingPlace(maxCampingIdx);
+	}
+
+	public void deleteCampingInternal(int camping_idx) {
+		campingDAO.deleteCampingInternal(camping_idx);
+	}
+
+	public void deleteCampingSafetyDevice(int camping_idx) {
+		campingDAO.deleteCampingSafetyDevice(camping_idx);
+	}
+
+	public void deleteCampingSite(int camping_idx) {
+		campingDAO.deleteCampingSite(camping_idx);
+	}
+
+	public CampingDTO selectCamping(int camping_idx) {
+		return campingDAO.selectCamping(camping_idx);
+	}
+
+	public CampingDTO selectCampingTwo(int camping_idx) {
+		return campingDAO.selectCampingTwo(camping_idx);
+	}
+
+	public int updateInternal(CampingDTO dto) {
+		return campingDAO.updateInternal(dto);
+	}
+
+	public int updateSafety(CampingDTO dto) {
+		return campingDAO.updateSafety(dto);
+	}
+
+	public int updateSite(CampingDTO dto) {
+		return campingDAO.updateSite(dto);
+	}
+
+	public CampingDTO selectCampingThree(int camping_idx) {
+		return campingDAO.selectCampingThree(camping_idx);
+	}
+
+	public CampingDTO selectOneByImg(int maxCampingIdx) {
+		return campingDAO.selectOneByImg(maxCampingIdx);
+	}
+
+	public CampingDTO selectInnerImg(int camping_idx) {
+		return campingDAO.selectInnerImg(camping_idx);
+	}
+
+	// 캠핑장명만 가져오기
+	public List<CampingDTO> selectFacltnmList() {
+		return campingDAO.selectFacltnmList();
+	}
+
+	public void deleteFile(CampingDTO storedImg) {
+		fileComponent.deleteFile(storedImg.getFirst_img());
+		String[] fileName = storedImg.getInner_img().split(",");
+		for (int i = 0; i < fileName.length; i++) {
+			fileComponent.deleteFile2(fileName[i]);
+		}
+	}
+}
---
+++ yourCamping-11.06-2/src/main/webapp/WEB-INF/views/board/freeView.jsp
@@ -0,0 +1,100 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ include file="../header.jsp"%>
+
+<div class="container mt-3">
+	<div class="w-75 mx-auto">
+		<div class="d-flex justify-content-between border-bottom border-secondary border-3 pt-4 pb-4">
+			<div class="col text-start ms-2">${dto.userid }</div>
+			<div class="col text-center">${dto.free_wdate }</div>
+			<div class="col text-end me-2">조회 : ${dto.free_viewCount }</div>
+		</div>
+		<div class="mt-5 mb-5">
+			<h2>${dto.free_title }</h2>
+		</div>
+		<div class="border-bottom border-secondary border-1">
+			<p class="mb-5">
+          		${dto.free_content }
+			</p>
+        	<c:if test="${dto.free_img != null}">
+        	<div class="mt-5 mb-5">
+        		<img src="${cpath }/upload/${dto.free_img }" class="d-block w-100">
+			</div>
+			</c:if>
+		</div>
+      	<div class="mt-4 mb-4 d-flex justify-content-end">
+      		<a href="${cpath }/board/freeModify/${dto.free_table_idx}"><button id="modifyBtn" type="button" class="btn btn-outline-secondary me-3">수정하기</button></a>
+      		<a href="${cpath }/board/freeDelete/${dto.free_table_idx}"><button id="deleteBtn" type="button" class="btn btn-outline-secondary me-3">삭제하기</button></a>
+        	<a href="${cpath }/board/freeList"><button type="button" class="btn btn-outline-secondary me-3">목록으로</button></a>
+      	</div>
+		<div class="mt-4">
+			<div class="mb-4">댓글</div>
+			<div class="ms-3 mb-3">
+				<c:forEach var="reply" items="${list }">
+				<div class="mb-3 d-flex justify-content-between">
+					<div>
+						<div class="d-flex mb-2">
+							<div class="me-2">${reply.userid }</div>
+							<div>${reply.reply_wdate}</div>
+						</div>
+						<div class="ms-2">${reply.reply_content}</div>
+					</div>
+					<div>
+						<a href="${cpath }/board/freeView/${dto.free_table_idx}/deleteReply"><button id="deleteReplyBtn" type="button" class="btn btn-outline-secondary">X</button></a>
+					</div>
+				</div>
+				</c:forEach>
+			</div>
+		</div>
+		<div class="mt-5 d-flex">
+			<form method="POST" class="d-flex w-100">
+				<textarea class="form-control me-4" rows="3" id="reply_content" placeholder="명예훼손, 개인정보 유출, 분쟁 유발, 허위사실 유포 등의 글은 이용약관에 의해 제재는 물론 법률에 의해 처벌받을 수 있습니다." name="reply_content" required></textarea>
+				<button type="submit" class="btn btn-secondary ms-4 text-nowrap ps-4 pe-4">등 록</button>
+			</form>
+		</div>
+	</div>
+</div>
+
+<script>
+	const modifyBtn = document.getElementById('modifyBtn')
+	const modifyHandler = function(event) {
+		event.preventDefault()			// 이벤트 기본 작동을 막는다
+		if ('${dto.userid}' != '${login.userid}') {
+			alert('본인 글만 수정할 수 있습니다')
+			return
+		}
+		if (confirm('수정하시겠습니까')) {
+			location.href = event.target.parentNode.href
+		}	
+	}
+	modifyBtn.onclick = modifyHandler
+
+	const deleteBtn = document.getElementById('deleteBtn')
+	const deleteHandler = function(event) {
+		event.preventDefault()			// 이벤트 기본 작동을 막는다
+		if ('${dto.userid}' != '${login.userid}') {
+			alert('본인 글만 삭제할 수 있습니다')
+			return
+		}
+		if (confirm('삭제하시겠습니까')) {
+			location.href = event.target.parentNode.href
+		}	
+	}
+	deleteBtn.onclick = deleteHandler
+	
+	const deleteReplyBtn = document.getElementById('deleteReplyBtn')
+	const deleteReplyHandler = function(event) {
+		event.preventDefault()			// 이벤트 기본 작동을 막는다
+		if ('${dto.userid}' != '${login.userid}') {
+			alert('본인 댓글만 삭제할 수 있습니다')
+			return
+		}
+		if (confirm('삭제하시겠습니까')) {
+			location.href = event.target.parentNode.href
+		}	
+	}
+	deleteReplyBtn.onclick = deleteReplyHandler
+</script>
+
+</body>
+</html>
