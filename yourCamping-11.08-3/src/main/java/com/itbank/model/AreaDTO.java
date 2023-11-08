428418c777835ac54fc8ce9ac9aacfccd6c120e6
RightAccept <djfltrhkdeo46@gmail.com>
2023-11-09 02:17:30 +0900

    3차 백업
    11월 8일
    1. view 페이지
    a. 캠핑장 정보가 나오는 <table class="table" style="width=400px;">의 width 100%로 변경(108줄)
    b. 예약하기의 View calender 문구 삭제
    c. bookmark 수정
    -> 추가 후 다시 클릭했을 때 삭제가 되지 않고 추가가 되던 현상 수정
    -> if (camping_list.includes('${dto.camping_idx}')를 isClicked로 변경했음
    
    2. boardController, boardService
    a. 검색 유형(작성자, 제목) 등을 Controller에서 구분하여 Service로 전달하는 형식이 아닌, Service에서 구분하도록 변경
    -> Controller에서 type과 keyword를 동시에 Service로 넘긴다
    
    3. board-mapper
    a. freeSearch와 reviewSearch 검색 구문 조금 변경
    -> '%'||#{keyword}||'%'에서 '%${keyword}%'로 변경
    
    4. 배포 시 문제점 수정
    a. 메인으로 이동시킬 때 사용하는 "${cpath}"를 그대로 두면 배포 시에는 메인으로 이동이 안된다
    -> "${cpath}/"로 변경
    
    5. 후기글 추천
    a. ReviewDTO를 삭제하고, UserDTO에 List<Integer> review_idx 추가
    b. login시 추천한 게시글을 확인하기 위해 review_idx를 받아온다
    -> userController, userService, UserDAO, user-mapper 수정
    c. reviewView에 script 작성
    -> 마우스 over시 바뀌고, 내리면 원래대로/클릭 시 고정 및 db에 추가
    d. ajaxController에 메서드 3개 추가(리뷰 추천 추가/제거, 추천 수 받아오기)
    -> BoardDAO에 메서드 3개 추가
    
    6. 회원탈퇴
    a. mypage_modify.jsp에 스크립트 추가
    -> 추후 mypage로 이동
    b. UserController, UserService, UserDAO, user-mapper.xml 추가

---
+++ yourCamping-11.08-3/src/main/java/com/itbank/model/AreaDTO.java
@@ -0,0 +1,20 @@
+package com.itbank.model;
+
+public class AreaDTO {
+	
+	private String areaNm;
+	private String areaCode;
+	
+	public String getAreaNm() {
+		return areaNm;
+	}
+	public void setAreaNm(String areaNm) {
+		this.areaNm = areaNm;
+	}
+	public String getAreaCode() {
+		return areaCode;
+	}
+	public void setAreaCode(String areaCode) {
+		this.areaCode = areaCode;
+	}
+}
---
+++ yourCamping-11.08-3/src/main/java/com/itbank/service/BoardService.java
@@ -0,0 +1,159 @@
+package com.itbank.service;
+
+import java.util.HashMap;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.itbank.component.BoardComponent;
+import com.itbank.component.FileComponent;
+import com.itbank.model.FreeDTO;
+import com.itbank.model.ReplyDTO;
+import com.itbank.model.ReviewDTO;
+
+@Service
+public class BoardService {
+
+	@Autowired private BoardComponent boardComponent;
+	@Autowired private FileComponent fileComponent;
+	
+	public List<ReviewDTO> selectReviewList() {
+		return boardComponent.selectReviewList();
+	}
+	
+	public int insertReview(ReviewDTO dto) {
+		if (dto.getUpload() != null) {
+			String innerFileName = "";
+			for(int i = 0; i < dto.getUpload().length; i++) {
+				String fileName = fileComponent.upload3(dto.getUpload()[i]);
+				innerFileName += fileName;
+				if (i < dto.getUpload().length - 1) {
+					innerFileName += ",";
+				}
+			}
+			dto.setReview_img(innerFileName);
+		}
+		return boardComponent.insertReview(dto);
+	}
+	
+	public ReviewDTO selectReviewOne(int review_idx) {
+		return boardComponent.selectReviewOne(review_idx);
+	}
+	
+	public int countReviewView(int review_idx) {
+		return boardComponent.countReviewView(review_idx);
+	}
+	
+	public int reviewDelete(int review_idx) {
+		String fileNames = boardComponent.selectReviewImg(review_idx);
+		if (fileNames != null) {
+			String[] arr = fileNames.split(",");
+			for (int i = 0; i < arr.length; i++) {
+				fileComponent.deleteFile3(arr[i]);
+			}
+		}
+		return boardComponent.reviewDelete(review_idx);
+	}
+	
+	public int reviewModify(ReviewDTO dto) {
+		return boardComponent.reviewModify(dto);
+	}
+	
+	public List<FreeDTO> selectFreeList() {
+		return boardComponent.selectFreeList();
+	}
+	
+	public int insertFree(FreeDTO dto) {
+		if (dto.getUpload().isEmpty() == false) {
+			String fileName = fileComponent.upload3(dto.getUpload());
+			dto.setFree_img(fileName);
+		}
+		return boardComponent.insertFree(dto);
+	}
+
+	public FreeDTO selectFreeOne(int free_table_idx) {
+		return boardComponent.selectFreeOne(free_table_idx);
+	}
+	
+	public int countFreeView(int free_table_idx) {
+		return boardComponent.countFreeView(free_table_idx);
+	}
+	
+	public int replyCount(int free_table_idx) {
+		return boardComponent.replyCount(free_table_idx);
+	}
+	
+	public List<ReplyDTO> selectReply(int free_table_idx) {
+		return boardComponent.selectReply(free_table_idx);
+	}
+
+	public int insertReply(ReplyDTO dto) {
+		return boardComponent.insertReply(dto);
+	}
+	
+	public int deleteReplyOne(ReplyDTO dto) {
+		return boardComponent.deleteReplyOne(dto);
+	}
+	
+	public int deleteReplyAll(int free_table_idx) {
+		return boardComponent.deleteReplyAll(free_table_idx);
+	}
+	
+	public int freeDelete(int free_table_idx) {
+		String fileName = boardComponent.selectFreeImg(free_table_idx);
+		if (fileName != null) {
+			fileComponent.deleteFile3(fileName);
+		}
+		return boardComponent.freeDelete(free_table_idx);
+	}
+
+	public int freeModify(FreeDTO dto) {
+		return boardComponent.freeModify(dto);
+	}
+	
+	public List<ReviewDTO> reviewSortViewCount() {
+		return boardComponent.reviewSortViewCount();
+	}
+	
+	public List<FreeDTO> freeSortViewCount() {
+		return boardComponent.freeSortViewCount();
+	}
+
+	public ReplyDTO selectReplyOne(HashMap<String, Object> map) {
+		return boardComponent.selectReplyOne(map);
+	}
+
+	public List<ReviewDTO> selectSearchReview(String type, String keyword) {
+		List<ReviewDTO> list = null;
+		switch(type) {
+		case "facltnm":
+			list = boardComponent.selectSearchReviewCamping(keyword);
+			break;
+		case "title":
+			list = boardComponent.selectSearchReviewTitle(keyword);
+			break;
+		case "writer":
+			list = boardComponent.selectSearchReviewWriter(keyword);
+			break;
+		default:
+			list = boardComponent.selectReviewList();	
+		} 
+		return list;
+	}
+
+	public List<FreeDTO> selectSearchFree(String type, String keyword) {
+		List<FreeDTO> list = null;
+		switch(type) {
+		case "title":
+			list = boardComponent.selectSearchFreeTitle(keyword);
+			break;
+		case "writer":
+			list = boardComponent.selectSearchFreeWriter(keyword);
+			break;
+		default:
+			list = boardComponent.selectFreeList();	
+		} 
+		return list;
+	}
+}
